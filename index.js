14371549b3ffa81d3be156b8cfbe107dd1518423
Marabrie <mararbrie@gmail.com>
2021-09-21 09:51:43 -0400

    added navbar and burgerIcon

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// mobile menu support
+// mobile menu
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
